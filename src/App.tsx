9f7a8e4dc1d2a505cc0c28c9f17f134b4c9b921b
eduardoGChavez <druidasco@gmail.com>
2024-03-21 23:30:37 -0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -19,8 +19,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/weather" element={<Weather />} />
-          {/* <Route path="*" element={<NotFound />} /> */}
-          <Route element={<NotFound />} />
+          <Route path="/*" element={<NotFound />} />
         </Routes>
       </Layout>
     </BrowserRouter>
